bcf54b2fe25bfb6acff507255e2940f2b3c3fe93
Ivan Arsenev <76220709+IvanArsenev@users.noreply.github.com>
2022-11-30 14:47:09 +0700

    Add files via upload

---
+++ rsa_ru_en.py
@@ -0,0 +1,55 @@
+def euler(n):
+    f = n
+    if n%2 == 0:
+        while n%2 == 0:
+            n = n // 2
+        f = f // 2
+    i = 3
+    while i*i <= n:
+        if n%i == 0:
+            while n%i == 0:
+                n = n // i
+            f = f // i
+            f = f * (i-1)
+        i = i + 2
+    if n > 1:
+        f = f // n
+        f = f * (n-1)
+    return f
+def keys(d_type):
+    if d_type == 1:
+        d, n = map(int, input("Введите закрытый ключ: ").split())
+    elif d_type == 2:
+        e, n = map(int, input("Введите открытый ключ: ").split())
+        for i in range(1, n):
+            if (i*e)%euler(n) == 1:
+                d = i
+                print(f"Закрытый ключ: {d} {n}")
+                break
+    return str(f'{d} {n}')
+def decode():
+    d = 0
+    bias = int(input("Смещение:\nРусский язык - 1039\nАнглийский язык - 64\nВведите код: "))
+    code = [int(x) for x in input("Введите вашу строку (заглавные бувы): ").split()]
+    d, n = keys(int(input("Режим дешифровки:\n1) Знаю закрытый ключ\n2) Знаю открытый ключ\nВыберите: "))).split()
+    d, n = int(d), int(n)
+    s = (chr(bias+(code[0]**d)%n))
+    for i in range(1,len(code)):
+        if ((code[i]**d)%n-code[i-1]) < 0:
+            s += (chr(bias+(code[i]**d)%n-code[i-1]+n))
+        else:
+            s += (chr(bias+(code[i]**d)%n-code[i-1]))
+    print(f"Расшифрованная строка: {s}")
+def encode():
+    bias = int(input("Смещение:\nРусский язык - 1039\nАнглийский язык - 64\nВведите код: "))
+    string = str(input('Введите вашу строку (заглавные бувы): '))
+    e, n = map(int, input("Введите открытый ключ: ").split())
+    coded_s = ( (ord(string[0]) - bias) ** e ) % n
+    s = str(coded_s)+' '
+    for i in range(1, len(string)):
+        s += str(((ord(string[i])-bias+coded_s)**e)%n) + ' '
+        coded_s = ((ord(string[i])-bias+coded_s)**e)%n
+    print(f"Зашифрованная строка: {s}")
+code_type = int(input("Что сделать?:\n1) Расшифровать\n2) Зашифровать\nВыберите: "))
+if code_type == 1: decode()
+else: encode()
